e38a05a0fb322fdbd7d2938248b7afa5422586fc
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-10-06 01:24:27 +0300

    Work on TUI

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
-#![windows_subsystem = "windows"] // Disable console window on Windows
-
-use templateer::GeneratorApp;
+use miette::{IntoDiagnostic, Result};
+use ratatui::prelude::*;
+use ratatui::widgets::{Block, Paragraph};
+use tui_textarea::{Input, Key, TextArea};
 
 #[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Templateer",
-        options,
-        Box::new(|cc| Box::new(GeneratorApp::new(cc))),
-    )
+fn main() -> Result<()> {
+    // Set up Crossterm
+    // TODO: Is raw mode needed? https://ratatui.rs/concepts/backends/raw-mode.html
+    // TODO: https://github.com/veeso/tui-realm-stdlib
+    crossterm::terminal::enable_raw_mode()
+        .into_diagnostic()?;
+    crossterm::execute!(std::io::stderr(), crossterm::terminal::EnterAlternateScreen)
+        .into_diagnostic()?;
+
+    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))
+        .into_diagnostic()?;
+    let mut text_area = TextArea::default();
+    text_area.set_block(Block::default().title("My text"));
+
+    loop {
+        terminal.draw(|f| {
+            let layout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![
+                    Constraint::Percentage(50),
+                    Constraint::Percentage(50),
+                ])
+                .split(f.size());
+
+            f.render_widget(Paragraph::new("Hello, world"), layout[0]);
+            f.render_widget(text_area.widget(), layout[1]);
+        }).into_diagnostic()?;
+
+        match crossterm::event::read().into_diagnostic()?.into() {
+            Input { key: Key::Tab, .. } => todo!(),
+            input => text_area.input(input),
+        };
+    }
+
+    // Clean up
+    crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen)
+        .into_diagnostic()?;
+    crossterm::terminal::disable_raw_mode()
+        .into_diagnostic()?;
+
+    Ok(())
 }
